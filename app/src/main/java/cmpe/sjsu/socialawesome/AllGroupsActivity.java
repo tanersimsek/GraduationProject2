290d637de37baf22197a9639765a459fbb1d43fb
taner <tnrsmsk@yandex.com>
2017-12-22 22:52:23 +0300

    grup oluştur eklendi allgroups a başlanacak 222

--- app/src/main/java/cmpe/sjsu/socialawesome/AllGroupsActivity.java
+++ app/src/main/java/cmpe/sjsu/socialawesome/AllGroupsActivity.java
@@ -9,5 +9,6 @@ public class AllGroupsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_groups);
+
     }
 }
